8feede4a26d796f4e4faa61aef844c78a5c9db73
カグチ <59391215+KagChi@users.noreply.github.com>
2020-10-18 15:39:45 +0700

    Create nezukoClient.ts

---
+++ src/handle/nezukoClient.ts
@@ -0,0 +1,27 @@
+import { Client, Collection, Message } from 'discord.js';
+import * as config from '../config.json';
+interface ICommand {
+    name: string;
+    aliases: string[];
+    cooldown: number;
+    guildOnly: boolean;
+    description: string;
+    usage: string[];
+    execute: (message: Message, args: string[], client: nezukoClient) => unknown; 
+ }
+
+class nezukoClient extends Client {
+    public helps: Collection<string, string> = new Collection();
+    public color: string = "#fafcc2";
+    public commands: Collection<string, ICommand> = new Collection();
+    public aliases: Collection<string, string> = new Collection();
+    public config: typeof config = config;
+    
+
+
+     
+    
+
+}
+
+export default nezukoClient;
