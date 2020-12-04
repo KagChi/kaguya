d32db533fb2af97f2c4e8ada41bd605abba0a7b1
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-04 19:00:38 +0700

    fix: typo

--- src/events/bot/messageDelete.ts
+++ src/events/bot/messageDelete.ts
@@ -1,4 +1,4 @@
 import { Message } from 'discord.js-light'
-export default (client, message: Message) {
+export default (client, message: Message) => {
     client.snipe.set(message.channel.id, message)
  }
