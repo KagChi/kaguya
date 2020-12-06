176feda0cd0ab49af3f22d37fa4651dd8f234816
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-06 07:51:57 +0700

    Update resumeCommand.ts

--- src/commands/music/resumeCommand.ts
+++ src/commands/music/resumeCommand.ts
@@ -7,7 +7,8 @@ export default {
     async execute(message, args, client) {
         if(!message.member.voice.channel) return message.reply("to use this command please join voice channel");
         if (!client.player.getQueue(message)) return message.reply(`there are no music playing`);
-        if(client.player.isPlaying(message)) return message.reply("the music already playing!");
+        const serverQueue = client.player.queues.get(message.guild.id)
+        if(!serverQueue.pause) return message.reply("the music already playing!");
         client.player.resume(message)
         const embed = client.embed()
         .setDescription(`<a:yes:739409625090228275> | Resumed current queue!`)
