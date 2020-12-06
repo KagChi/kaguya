600fa65e8bcc45b5c26bcb9a71bd96fbd96e5e1f
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-06 11:26:48 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import nezukoClient from './handle/nezukoClient';
 const client = new nezukoClient({
     disableMentions: 'everyone',
     cacheGuilds: true,
-	cacheChannels: true,
-	cacheOverwrites: false,
-	cacheRoles: false,
-	cacheEmojis: false,
-	cachePresences: false,
-	ws: { properties: { $browser: "Discord iOS" } }
+    cacheChannels: true,
+    cacheOverwrites: false,
+    cacheRoles: false,
+    cacheEmojis: false,
+    cachePresences: false,
+    fetchAllMembers: true
+    ws: { properties: { $browser: "Discord iOS" } }
 });
 
 require('./handle/musicEvent').default(client)
