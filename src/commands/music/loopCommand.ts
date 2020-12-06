0d634ea03557abcce461ae623a7d25519f68e9dc
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-06 07:24:10 +0700

    Update loopCommand.ts

--- src/commands/music/loopCommand.ts
+++ src/commands/music/loopCommand.ts
@@ -3,9 +3,17 @@ export default {
     aliases: ["l"],
     cooldown: 2,
     guildOnly: true,
-    description: "play music",
+    description: "loop current queue",
     async execute(message, args, client) {
-       
+      if(!message.member.voice.channel) return message.reply("to use this command please join voice channel");
+      if (!client.player.getQueue(message)) return message.reply(`there are no music playing`);
+      let serverQueue = client.player.queues.get(message.guild.id);
+      serverQueue.repeatMode = !serverQueue.repeatMode
+      client.player.setRepeatMode(message, serverQueue.repeatMode)
+      let embed = client.embed()
+      .setDescription(`<a:yes:739409625090228275> | Loop set to \`${serverQueue.repeatMode ? "on" : "off"}\` `)
+      .setColor(client.color);
+      message.channel.send(embed);
 
  }
 }
