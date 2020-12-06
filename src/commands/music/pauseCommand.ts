1ea0061fde52d1e607230be7c2366298a7fead74
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-06 07:52:18 +0700

    Update pauseCommand.ts

--- src/commands/music/pauseCommand.ts
+++ src/commands/music/pauseCommand.ts
@@ -8,7 +8,7 @@ export default {
     if(!message.member.voice.channel) return message.reply("to use this command please join voice channel");
     if (!client.player.getQueue(message)) return message.reply(`there are no music playing`);
     const serverQueue = client.player.queues.get(message.guild.id)
-    if(!serverQueue.pause(message)) return message.reply("The music already paused!");
+    if(serverQueue.pause) return message.reply("The music already paused!");
     client.player.pause(message)
     const embed = client.embed()
     .setDescription(`<a:yes:739409625090228275> | Paused current queue!`)
