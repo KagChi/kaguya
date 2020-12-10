9890da6cebf846d3ae9b379b9d95c2f7755a8553
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 17:40:28 +0700

    Update utility.ts

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,6 +1,10 @@
-import { Client } from "discord.js-light";
+import { Client } from 'discord.js-light';
+import axios from 'axios';
 export default class Utility {
    public constructor(public readonly client : Client){}
 
-   
+   public hastebin(text: string){
+   const { data } = await axios.post('https://bin.nezukochan.xyz/documents, { payload: text })
+   return `https://bin.nezukochan.xyz/${data}.js`
+  }
 }
