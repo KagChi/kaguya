9bc1d122cb19592ea5ae05f2a0d269f5f8f0269a
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 19:18:34 +0700

    Create pingCommand.ts

---
+++ src/commands/Utility/pingCommand.ts
@@ -0,0 +1,20 @@
+import type { Message } from "discord.js-light";
+import { CommandConf } from "../../decorators";
+import Command from "../../structures/Command";
+
+@CommandConf({ 
+    name: "ping",
+    aliases: [""],
+    description: "get bot latency",
+    usage: "",
+    cooldown: 3,
+    ownerOnly: false
+})
+
+export default class PingCommand extends Command {
+    public async exec(msg: Message, args: string[]): Promise<void> {
+    const embed = client.embed()
+    .addField("Pong!!!!", client.ws.ping)
+    msg.channel.send(embed)
+   }
+ }
