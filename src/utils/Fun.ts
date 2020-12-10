8802143682576a5c3802df939c5b991ca9631663
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 20:59:02 +0700

    Create Fun.ts

---
+++ src/utils/Fun.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const baseURL = "https://nekos.life/api/v2/img";
+export default class Fun {
+     public async trap(){
+       const { data } = await axios.get(baseURL + "trap")
+       return data.url
+     }
+ }
