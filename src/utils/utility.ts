538ca3273cc6ecf1883719389198df7739ad0c6c
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 18:42:45 +0700

    Update utility.ts

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class Utility {
    public constructor(public readonly client : Client){}
    public color: string = "EE2677"
-   public embed = () => new MessageEmbed()
+   public embed: MessageEmbed = () => new MessageEmbed()
    public async hastebin(text: string){
    const { data } = await axios.post('https://bin.nezukochan.xyz/documents', { payload: text })
    return `https://bin.nezukochan.xyz/${data}.js`
