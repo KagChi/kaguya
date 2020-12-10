10adf6f008fd41d385ea1fc5dab4c3caecd89598
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 18:40:32 +0700

    Update utility.ts

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,8 +1,9 @@
-import { Client } from 'discord.js-light';
+import { Client, MessageEmbed() } from 'discord.js-light';
 import axios from 'axios';
 export default class Utility {
    public constructor(public readonly client : Client){}
-
+   public color: string = "EE2677"
+   public embed = () => new MessageEmbed()
    public async hastebin(text: string){
    const { data } = await axios.post('https://bin.nezukochan.xyz/documents', { payload: text })
    return `https://bin.nezukochan.xyz/${data}.js`
