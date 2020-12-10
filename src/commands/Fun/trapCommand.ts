7cb090d184511324b68dd3d63051ab63aa3e4c9d
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 21:07:04 +0700

    Create trapCommand.ts

---
+++ src/commands/Fun/trapCommand.ts
@@ -0,0 +1,21 @@
+import type { Message } from "discord.js-light";
+import { CommandConf } from "../../decorators";
+import Command from "../../structures/Command";
+@CommandConf({ 
+    name: "trap",
+    aliases: [""],
+    description: "random trap image 🗿",
+    usage: "",
+    cooldown: 3,
+    ownerOnly: false
+})
+
+export default class TrapCommand extends Command {
+    public async exec(msg: Message, args: string[]): Promise<void> {
+      const embed = this.client.util.embed()
+      .setTitle("Random Trap Image :)")
+      .setImage(await this.client.fun.trap)
+      .setColor(this.client.util.color)
+      message.channel.send(embed)  
+  }
+ }
