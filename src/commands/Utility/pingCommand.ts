3f2ec35b42ad4bd6a8d6ceb4c3b1633cb9f9cc75
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 20:05:33 +0700

    Update pingCommand.ts

--- src/commands/Utility/pingCommand.ts
+++ src/commands/Utility/pingCommand.ts
@@ -1,7 +1,7 @@
 import type { Message } from "discord.js-light";
 import { CommandConf } from "../../decorators";
 import Command from "../../structures/Command";
-const delay = ms as number => new Promise(res => setTimeout(res, ms));
+const delay = ms: number => new Promise(res => setTimeout(res, ms));
 @CommandConf({ 
     name: "ping",
     aliases: [""],
