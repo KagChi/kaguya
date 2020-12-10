e794721db19e3befe8fedac6eaa9f9fe410a8424
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 21:34:46 +0700

    Create nekoCommand.ts

---
+++ src/commands/Fun/nekoCommand.ts
@@ -0,0 +1,22 @@
+import type { Message } from "discord.js-light";
+import { CommandConf } from "../../decorators";
+import Command from "../../structures/Command";
+@CommandConf({ 
+    name: "neko",
+    aliases: [""],
+    description: "random neko image",
+    usage: "",
+    cooldown: 3,
+    ownerOnly: false
+})
+
+export default class PokeCommand extends Command {
+    public async exec(msg: Message, args: string[]): Promise<void> {
+        const image = await this.client.fun.neko()
+      const embed = this.client.util.embed()
+      .setTitle("Random Neko Image :)")
+      .setImage(image)
+      .setColor(this.client.util.color)
+      msg.channel.send(embed)  
+  }
+ }
