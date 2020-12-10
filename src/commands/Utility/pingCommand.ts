7d8714e5abf7446c0502df69c5e3191fa0519fd4
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 19:51:57 +0700

    Update pingCommand.ts

--- src/commands/Utility/pingCommand.ts
+++ src/commands/Utility/pingCommand.ts
@@ -1,7 +1,7 @@
 import type { Message } from "discord.js-light";
 import { CommandConf } from "../../decorators";
 import Command from "../../structures/Command";
-
+const delay = ms => new Promise(res => setTimeout(res, ms));
 @CommandConf({ 
     name: "ping",
     aliases: [""],
@@ -13,12 +13,14 @@ import Command from "../../structures/Command";
 
 export default class PingCommand extends Command {
     public async exec(msg: Message, args: string[]): Promise<void> {
-        try {
+        const message = await msg.channel.send("Getting info...");
     const embed = this.client.util.embed()
-    .addField("Pong!!!!", this.client.ws.ping)
-    msg.channel.send(embed)
-   } catch(e) {
-     return msg.channel.send(`Oh no an error occured :( ${e.message} try again later`);
-   }
+    .setColor(client.color)
+    .addField(`⏳ ${lattency} `, `__**${m.createdTimestamp - msg.createdTimestamp}ms**__`)
+    .addField("💓 API", `__**${Math.floor(this.client.ws.ping)}ms**__`)
+    .setTimestamp();
+    await delay(5000)
+        message.edit('',embed)
+   
   }
  }
