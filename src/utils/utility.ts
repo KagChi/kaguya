682175f9ea3df78ba14f7dfb3504147126d1a31c
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 17:26:40 +0700

    Update utility.ts

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,11 +1,6 @@
 import { Client } from "discord.js-light";
-import brainly from 'brainly-scraper-v2'
-import type brainlyCountry from 'brainly-scraper-v2'
 export default class Utility {
    public constructor(public readonly client : Client){}
 
-   public async brainLess(query: string, length=5, country="id") {
-    const data = await brainly(query, length, country)
-    return data
-   }
+   
 }
