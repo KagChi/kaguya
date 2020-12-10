2a5448dccf270adf0724e5582490313db1c70d53
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 20:43:51 +0700

    i hope fixed

--- src/commands/Utility/pingCommand.ts
+++ src/commands/Utility/pingCommand.ts
@@ -1,7 +1,7 @@
 import type { Message } from "discord.js-light";
 import { CommandConf } from "../../decorators";
 import Command from "../../structures/Command";
-const delay = ms as number => new Promise(res => setTimeout(res, ms));
+const delay = ms as number; => new Promise(res => setTimeout(res, ms));
 @CommandConf({ 
     name: "ping",
     aliases: [""],
@@ -13,7 +13,14 @@ const delay = ms as number => new Promise(res => setTimeout(res, ms));
 
 export default class PingCommand extends Command {
     public async exec(msg: Message, args: string[]): Promise<void> {
-   
+   const message = await msg.channel.send("Getting info...");
+    const embed = this.client.util.embed()
+    .setColor(this.client.util.color)
+    .addField(`⏳ Latency `, `__**${message.createdTimestamp - msg.createdTimestamp}ms**__`)
+    .addField("💓 API", `__**${Math.floor(this.client.ws.ping)}ms**__`)
+    .setTimestamp();
+     await delay(5000);
+      message.edit('',embed);
    
   }
  }
