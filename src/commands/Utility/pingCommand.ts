2695f5046c9b38e1c8649894f01d05902d98ca9c
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-10 20:01:49 +0700

    Update pingCommand.ts

--- src/commands/Utility/pingCommand.ts
+++ src/commands/Utility/pingCommand.ts
@@ -13,14 +13,14 @@ const delay = ms as number => new Promise(res => setTimeout(res, ms));
 
 export default class PingCommand extends Command {
     public async exec(msg: Message, args: string[]): Promise<void> {
-        const message = await msg.channel.send("Getting info...");
+   const message = await msg.channel.send("Getting info...");
     const embed = this.client.util.embed()
     .setColor(this.client.util.color)
     .addField(`⏳ Latency `, `__**${message.createdTimestamp - msg.createdTimestamp}ms**__`)
     .addField("💓 API", `__**${Math.floor(this.client.ws.ping)}ms**__`)
     .setTimestamp();
-    await delay(5000);
-        message.edit('',embed)
+     await delay(5000);
+      message.edit('',embed)
    
   }
  }
