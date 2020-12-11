5b3296177dc1826282fa538b9dd0e4448b4caee9
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-11 21:17:15 +0700

    Update playCommand.ts

--- src/commands/Music/playCommand.ts
+++ src/commands/Music/playCommand.ts
@@ -42,7 +42,7 @@ export default class playCommand extends Command {
                 connection: null as any,
                 songs: [] as string[],
                 loop: false,
-                filters: null as any,
+                filters: [] as any,
                 volume: 100,
                 playing: true
               };
