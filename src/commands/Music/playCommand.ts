edea5adac93f997ee8abadc4116c87624935c3db
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-11 21:12:18 +0700

    Update playCommand.ts

--- src/commands/Music/playCommand.ts
+++ src/commands/Music/playCommand.ts
@@ -48,6 +48,7 @@ export default class playCommand extends Command {
               };
             Object.keys(filters).forEach((f) => {
             queueConstruct.filters[f] = false
+             })
               const songModel: any = {
                   id: song[0].id,
                   title: song[0].title,
