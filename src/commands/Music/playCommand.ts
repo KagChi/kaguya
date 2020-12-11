8e090f03e69651641ca1179ad3d9a8e0019ac927
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-11 12:27:53 +0700

    Update playCommand.ts

--- src/commands/Music/playCommand.ts
+++ src/commands/Music/playCommand.ts
@@ -1,6 +1,26 @@
 import type { Guild, Message } from "discord.js-light";
 import { CommandConf } from "../../decorators";
 import Command from "../../structures/Command";
+const filters: any = {
+    bassboost: 'bass=g=20,dynaudnorm=f=200',
+    '8D': 'apulsator=hz=0.08',
+    vaporwave: 'aresample=48000,asetrate=48000*0.8',
+    nightcore: 'aresample=48000,asetrate=48000*1.25',
+    phaser: 'aphaser=in_gain=0.4',
+    tremolo: 'tremolo',
+    vibrato: 'vibrato=f=6.5',
+    reverse: 'areverse',
+    treble: 'treble=g=5',
+    normalizer: 'dynaudnorm=f=200',
+    surrounding: 'surround',
+    pulsator: 'apulsator=hz=1',
+    subboost: 'asubboost',
+    karaoke: 'stereotools=mlev=0.03',
+    flanger: 'flanger',
+    gate: 'agate',
+    haas: 'haas',
+    mcompand: 'mcompand'
+}
 @CommandConf({ 
     name: "play",
     aliases: ["p"],
@@ -14,10 +34,31 @@ export default class playCommand extends Command {
     public async exec(msg: Message, args: string[]) {
         const query = args.join(" ")
         if(!query) return msg.channel.send("No query provided!");
-        const song = await this.client.musicManager.getSongs(query)
-        const serverQueue = this.client.queue.get(msg.guild?.id as Guild["id"])
+        const song = await this.client.musicManager.getSongs(query) as any
+        const serverQueue = this.client.queue.get(msg.guild?.id as Guild["id"]) as any
+        const queueConstruct = {
+                textChannel: msg.channel,
+                voiceChannel: msg.member?.voice.channel,
+                connection: null as any,
+                songs: [] as string[],
+                loop: false,
+                filters: filters,
+                volume: 100,
+                playing: true
+              };
         if(serverQueue){
-            
+              const songModel: any = {
+                  id: song[0].id,
+                  title: song[0].title,
+                  thumbnail: song[0].thumbnail.url,
+                  duration: song[0].duration,
+                  durationFormatted: song[0].durationFormatted,
+                  url: `https://www.youtube.com/watch?v=${song[0].id}`
+              }
+             serverQueue.songs.push(song);
+            serverQueue.textChannel.send(`✅ **${song.title}** has been added to the queue by ${msg.author}`)
+        } else {
+            queueConstruct.songs.push(song);
         }
         return this.client.musicManager.handleVideo(msg, song[0] as unknown as string[])
     }
