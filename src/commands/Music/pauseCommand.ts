68c11219a931917cd80a1d4d16325e2a63608f7a
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-12 13:17:57 +0700

    fix

--- src/commands/Music/pauseCommand.ts
+++ src/commands/Music/pauseCommand.ts
@@ -13,8 +13,21 @@ import Command from "../../structures/Command";
 export default class pauseCommand extends Command {
     public async exec(msg: Message, args: string[]) {
         const voiceChannel = msg.member?.voice.channel
+        const serverQueue = this.client.queue.get(msg.guild?.id as Guild["id"]) as any
         if (!voiceChannel) return msg.channel.send("You must join voiceChannel first");
         if (this.client.queue.has(msg.guild?.id as Guild["id"]) && voiceChannel.id !== this.client.queue.get(msg.guild?.id as Guild["id"])?.voiceChannel.id)return msg.channel.send(`You must be in **${this.client.queue?.get(msg.guild?.id as Guild["id"])?.voiceChannel.name}** to pause music`);
+        const noQueue = this.client.util.embed()
+        .setTitle("Error!")
+        .setDescription("There are no music playing")
+        .setColor(this.client.util.color)
+        .setThumbnail(msg.author?.avatarURL({ dynamic: true }) as any)
+        if(!serverQueue) return msg.channel.send(noQueue);
+        const noPlayingEmbed = this.client.util.embed()
+        .setTitle("Error!")
+        .setDescription("I can't pause the song that paused")
+        .setColor(this.client.util.color)
+        .setThumbnail(msg.author?.avatarURL({ dynamic: true }) as any)
+        if(!serverQueue.playing) return msg.channel.send(noPlayingEmbed);
         this.client.musicManager.pause(msg)
         const embed = this.client.util.embed()
         .setTitle("Paused current queue")
