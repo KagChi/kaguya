7441b24876c9803e0e93c1e3929a0bc217c62e1e
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-14 18:35:11 +0700

    Update musicManager.ts

--- src/utils/musicManager.ts
+++ src/utils/musicManager.ts
@@ -3,27 +3,6 @@ import ytdl from "discord-ytdl-core";
 import YouTube from "youtube-sr";
 import YoutubePL = require("ytpl");
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-const filters: any = {
-    bassboost: 'bass=g=20,dynaudnorm=f=200',
-    '8D': 'apulsator=hz=0.08',
-    vaporwave: 'aresample=48000,asetrate=48000*0.8',
-    nightcore: 'aresample=48000,asetrate=48000*1.25',
-    phaser: 'aphaser=in_gain=0.4',
-    tremolo: 'tremolo',
-    vibrato: 'vibrato=f=6.5',
-    reverse: 'areverse',
-    treble: 'treble=g=5',
-    normalizer: 'dynaudnorm=f=200',
-    surrounding: 'surround',
-    pulsator: 'apulsator=hz=1',
-    subboost: 'asubboost',
-    karaoke: 'stereotools=mlev=0.03',
-    flanger: 'flanger',
-    gate: 'agate',
-    haas: 'haas',
-    mcompand: 'mcompand'
-}
-
 export default class musicManager {
     constructor(public readonly client : Client){}
     
