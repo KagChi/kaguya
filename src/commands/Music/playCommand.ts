6cc0a348cc98eedfffaa93c09d33cf3af33763ce
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-14 18:43:39 +0700

    Update playCommand.ts

--- src/commands/Music/playCommand.ts
+++ src/commands/Music/playCommand.ts
@@ -1,26 +1,6 @@
 import type { Guild, Message } from "discord.js-light";
 import { CommandConf } from "../../decorators";
 import Command from "../../structures/Command";
-const filters: any = {
-    bassboost: 'bass=g=20,dynaudnorm=f=200',
-    '8D': 'apulsator=hz=0.08',
-    vaporwave: 'aresample=48000,asetrate=48000*0.8',
-    nightcore: 'aresample=48000,asetrate=48000*1.25',
-    phaser: 'aphaser=in_gain=0.4',
-    tremolo: 'tremolo',
-    vibrato: 'vibrato=f=6.5',
-    reverse: 'areverse',
-    treble: 'treble=g=5',
-    normalizer: 'dynaudnorm=f=200',
-    surrounding: 'surround',
-    pulsator: 'apulsator=hz=1',
-    subboost: 'asubboost',
-    karaoke: 'stereotools=mlev=0.03',
-    flanger: 'flanger',
-    gate: 'agate',
-    haas: 'haas',
-    mcompand: 'mcompand'
-}
 @CommandConf({ 
     name: "play",
     aliases: ["p"],
@@ -51,7 +31,7 @@ export default class playCommand extends Command {
                 volume: 100,
                 playing: true
               };
-            Object?.keys(filters).forEach((f) => {
+            Object?.keys(this.client.util.filters).forEach((f) => {
             queueConstruct.filters[f] = false
              })
               const songModel: any = {
