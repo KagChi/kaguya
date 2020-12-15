53f4061a66697bb9cc5b66855c80e2086a16dfff
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-15 14:26:15 +0700

    Update createCommand.ts

--- src/commands/Playlist/createCommand.ts
+++ src/commands/Playlist/createCommand.ts
@@ -18,7 +18,7 @@ export default class createCommand extends Command {
             name: args[0]
         }) as any
         if(!args[0]) return msg.channel.send("Cant create playlist with empty name.");
-        if(playlist?.length > 5) return msg.channel.send("Cant create playlist more than 5!");
+        if(playlist?.length >= 5) return msg.channel.send("Cant create playlist more than 5!");
         if(playlist) return msg.reply(`${args[0]} Is already created, input another name!`);
         if(!playlist){
             const data = {
