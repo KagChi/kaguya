a30b7d46f3430c085ac8afcd2dbdc3934283c36c
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-18 16:03:18 +0700

    Update voiceStateUpdateEvent.ts

--- src/listeners/voiceStateUpdateEvent.ts
+++ src/listeners/voiceStateUpdateEvent.ts
@@ -16,7 +16,58 @@ export default class voiceStateUpdateEvent extends Listener {
            serverQueue?.textChannel.send(embed)
            this.client.queue.delete(oldState?.guild?.id as Guild["id"])
         }
-      }
+        const voiceChannel = serverQueue?.voiceChannel?.members.filter((x: any) => !x.user.bot)
 
-   
+        if (oldID === this.client.queue.get(oldState?.guild?.id as Guild["id"])?.voiceChannel?.id && newID !== this.client.queue.get(oldState?.guild?.id as Guild["id"])?.voiceChannel?.id && !newState?.member?.user.bot && serverQueue?.timeout === null) return this.timeoutQueue(voiceChannel, oldState);
+        if (newID === this.client.queue.get(newState?.guild?.id as Guild["id"])?.voiceChannel?.id && !newState?.member?.user.bot) return this.resume(voiceChannel, newState);
+    }
+
+    /**
+     * timeoutQueue
+     */
+    public async timeoutQueue(voiceChannel: any, state: VoiceState): Promise<void> {
+        if(voiceChannel?.size !== 0) return;
+        const serverQueue = this.client.queue.get(state?.guild?.id as Guild["id"]) as any
+        console.log(serverQueue)
+        serverQueue.playing = false
+        serverQueue.connection.dispatcher.pause()
+        serverQueue.timeout = setTimeout(async () => {
+            const deleteEmbed = this.client.util.embed()
+            .setColor(this.client.util.color)
+            .setTitle("Deleted Queue!")
+            .setDescription("Deleted Queue because i was alone for 15 seconds")
+            serverQueue.textChannel.send(deleteEmbed)
+           await serverQueue.voiceChannel.leave()
+            return this.client.queue.delete(state?.guild?.id as Guild["id"])
+        }, 15000)
+
+        const pauseEmbed = this.client.util.embed()
+        .setTitle("A User Have left the Voice Channel!")
+        .setDescription("Paused Current Queue\nif in 20 seconds no user join queue will deleted!")
+        .setColor(this.client.util.color)
+        serverQueue?.textChannel.send(pauseEmbed)
+    }
+
+    /**
+     * resume
+     */
+    public resume(voiceChannel: any, state: VoiceState): void {
+        const serverQueue = this.client.queue.get(state?.guild?.id as Guild["id"]) as any
+        if(voiceChannel?.size > 0){
+            if(voiceChannel?.size === 1) {
+                clearTimeout(serverQueue.timeout)
+                serverQueue.timeout = null
+            }
+            if(!serverQueue.playing) {
+                const resumeEmbed = this.client.util.embed()
+                .setTitle("A User Joined Voice Channel!")
+                .setDescription("I've resumed current queue!")
+                .setColor(this.client.util.color)
+                serverQueue.textChannel.send(resumeEmbed)
+                serverQueue.playing = true
+                serverQueue.connection.dispatcher.resume()
+            }
+        }
+        
+    }
 }
