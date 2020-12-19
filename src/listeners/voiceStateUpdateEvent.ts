a6b8e4bc1c5f597c0a090331967223e7ec4be903
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-19 21:17:47 +0700

    Update voiceStateUpdateEvent.ts

--- src/listeners/voiceStateUpdateEvent.ts
+++ src/listeners/voiceStateUpdateEvent.ts
@@ -18,8 +18,8 @@ export default class voiceStateUpdateEvent extends Listener {
         }
         const voiceChannel = serverQueue?.voiceChannel?.members.filter((x: any) => !x.user.bot)
 
-        if (oldID === serverQueue.voiceChannel?.id && newID !== serverQueue?.voiceChannel?.id && !newState?.member?.user.bot && serverQueue?.timeout === null) return this.timeoutQueue(voiceChannel, oldState);
-        if (newID === serverQueue?.voiceChannel?.id && !newState?.member?.user.bot) return this.resume(voiceChannel, newState);
+        if (oldID === this.client.queue.get(oldState?.guild?.id as Guild["id"])?.voiceChannel?.id && newID !== this.client.queue.get(oldState?.guild?.id as Guild["id"])?.voiceChannel?.id && !newState?.member?.user.bot && serverQueue?.timeout === null) return this.timeoutQueue(voiceChannel, oldState);
+        if (newID === this.client.queue.get(newState?.guild?.id as Guild["id"])?.voiceChannel?.id && !newState?.member?.user.bot) return this.resume(voiceChannel, newState);
     }
 
     /**
