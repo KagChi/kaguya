b4178098b195681a2ec397dd648669875346baa9
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-19 19:33:44 +0700

    Update playCommand.ts

--- src/commands/Music/playCommand.ts
+++ src/commands/Music/playCommand.ts
@@ -85,10 +85,10 @@ export default class playCommand extends Command {
         } else {
             queueConstruct.songs.push(songModel);
         }
-        msg.guild?.queue = queueConstruct;
         if(!serverQueue) this.client.queue.set(msg.guild?.id as Guild["id"], queueConstruct as any);
         if(!serverQueue) {
          try {
+                  msg.guild!.queue = queueConstruct;
                   const connection = await msg.member?.voice.channel?.join()
                   msg.guild?.me?.voice.setSelfDeaf(true)
                   queueConstruct.connection = connection
@@ -97,7 +97,7 @@ export default class playCommand extends Command {
                   msg.channel.send(`an error occured \`${e}\` `)
                   await msg.member?.voice.channel?.leave()
                   this.client.queue.delete(msg.guild?.id as Guild["id"])
-                  msg.guild?.queue = null;
+                  msg.guild!.queue = null;
               }
            }
         }
