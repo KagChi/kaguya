1679f4ab0be4a868a0ef9f59a493724845426d1d
カグチ <59391215+KagChi@users.noreply.github.com>
2020-12-21 13:36:24 +0700

    Update messageUpdateEvent.ts

--- src/listeners/messageUpdateEvent.ts
+++ src/listeners/messageUpdateEvent.ts
@@ -4,33 +4,35 @@ import Listener from "../structures/Listener";
 export default class MessageUpdateEvent extends Listener {
     public name = "messageUpdate";
     private readonly prefix = this.client.config.prefix;
-    public async exec(msg: Message): Promise<Message | void> {
-        if (!msg.guild) return;
-        if (msg.author.bot) return;
-        if (!msg.content.startsWith(this.prefix)) return;
-
-        const args = msg.content.slice(this.prefix.length).trim().split(/ +/g);
+    public async exec(oldMessage: Message, newMessage: Message): Promise<Message | void> {
+        if (oldMessage.content === newMessage.content) return undefined;
+        if (!newMessage.guild) return;
+        if (newMessage.author.bot) return;
+        if (!newMessage.content.startsWith(this.prefix)) return;
+        if (oldMessage.content === newMessage.content) return undefined;
+       
+        const args = newMessage.content.slice(this.prefix.length).trim().split(/ +/g);
         const commandName = args.shift()!.toLowerCase();
         const command = this.client.commands.get(commandName.toLowerCase()) || this.client.commands.find(c => c.config.aliases!.includes(commandName.toLowerCase()));
         if (command) {
-            if (command.config.ownerOnly && !msg.author.isDev) return;
-            if (!msg.author.isDev) {
+            if (command.config.ownerOnly && !newMessage.author.isDev) return;
+            if (!newMessage.author.isDev) {
                 const now = Date.now();
-                const userCooldown = this.client.cooldowns.get(`${command.config.name}-${msg.author.id}`);
+                const userCooldown = this.client.cooldowns.get(`${command.config.name}-${newMessage.author.id}`);
                 const cooldownAmount = command.config.cooldown! * 1000;
                 if (userCooldown) {
                     const expirationTime = userCooldown + cooldownAmount;
                     if (now < expirationTime) {
                         const timeLeft = (expirationTime - now) / 1000;
-                        await msg.channel.send(`Hold on, you just need to wait for ${timeLeft.toFixed(1)} secs to use \`${command.config.name}\` again.`);
+                        await newMessage.channel.send(`Hold on, you just need to wait for ${timeLeft.toFixed(1)} secs to use \`${command.config.name}\` again.`);
                         return;
                     }
                 }
-                this.client.cooldowns.set(`${command.config.name}-${msg.author.id}`, now);
-                setTimeout(() => this.client.cooldowns.delete(`${command.config.name}-${msg.author.id}`), cooldownAmount);
+                this.client.cooldowns.set(`${command.config.name}-${newMessage.author.id}`, now);
+                setTimeout(() => this.client.cooldowns.delete(`${command.config.name}-${newMessage.author.id}`), cooldownAmount);
             }
             try {
-                await command.exec(msg, args);
+                await command.exec(newMessage, args);
             } catch (e) {
                 console.error(e);
             }
