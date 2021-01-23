3727833122f45778274ac0049418a0be83f3321b
カグチ <59391215+KagChi@users.noreply.github.com>
2021-01-23 22:23:40 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv = require('dotenv');
 dotenv.config();
 const client = new Client();
 const app = require('express')()
-app.get('/', function (req, res) {
+app.get('/', function (req: any, res: any) {
   res.send('Hello World')
 })
 app.listen(3000)
