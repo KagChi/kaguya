bed8e31568f0a9cd38dcde26a4dbb604ddd86ffb
カグチ <59391215+KagChi@users.noreply.github.com>
2021-01-23 22:18:52 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import Client from './structures/kaguyaClient';
 import dotenv = require('dotenv');
 dotenv.config();
 const client = new Client();
+const app = require('express')()
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.listen(3000)
 client.run();
