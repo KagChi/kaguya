2a90f249a20557ebee68f89da0f526d471ef44ab
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-10 20:18:23 +0700

    chore: add requester to tracks

--- src/Commands/Util/playCommand.ts
+++ src/Commands/Util/playCommand.ts
@@ -1,5 +1,5 @@
 import { Command } from 'discord-akairo';
-import { Message, StageChannel, VoiceChannel } from 'discord.js';
+import { GuildMember, Message, StageChannel, VoiceChannel } from 'discord.js';
 import { CreateEmbed } from '../../Util/CreateEmbed';
 export default class PlayCommand extends Command {
     constructor() {
@@ -19,7 +19,7 @@ export default class PlayCommand extends Command {
         });
     }
     async exec(msg: Message, { musicName } : { musicName: string}) {
-        const tracks = await this.client.music.load(musicName);
+        const tracks = await this.client.music.getSongs(musicName, msg.member as GuildMember);
         const player = this.client.music.create(msg.member?.voice.channel as StageChannel | VoiceChannel);
         player?.connect();
         player?.play(tracks.tracks[0])
